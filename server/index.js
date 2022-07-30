04c690cafb669522fc7659561f5f04002b411e65
unknown <mdrakibulhoque83@gmail.com>
2022-07-30 15:35:02 +0600

    dot env write for secret credentials

--- server/index.js
+++ server/index.js
@@ -2,16 +2,18 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import morgan from "morgan";
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
+
 //MongoDB URL
-const uri =
-  "mongodb+srv://arcanerakib:ibphlvYNoM6ZjZbR@cluster0.emvk4y1.mongodb.net/travel_db?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.emvk4y1.mongodb.net/?retryWrites=true&w=majority`;
 
 const port = 5000;
 
