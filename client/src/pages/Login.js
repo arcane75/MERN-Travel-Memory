32b571a582cecdc0cd724533b10ca2c047f839bc
unknown <mdrakibulhoque83@gmail.com>
2022-08-01 12:15:19 +0600

    redux implement, successfully apply with register system

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -23,8 +23,15 @@ const initialState = {
 const Login = () => {
   const [formValue, setFormValue] = useState(initialState);
   const { email, password } = formValue;
+
+  const { loading, error } = useSelector((state) => ({ ...state.auth }));
+
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    error && toast.error(error);
+  }, [error]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -78,14 +85,14 @@ const Login = () => {
             </div>
             <div className="col-12">
               <MDBBtn style={{ width: "100%" }} className="mt-2">
-                {/* {loading && (
+                {loading && (
                   <MDBSpinner
                     size="sm"
                     role="status"
                     tag="span"
                     className="me-2"
                   />
-                )} */}
+                )}
                 Login
               </MDBBtn>
             </div>
--- client/src/redux/features/authSlice.js
+++ client/src/redux/features/authSlice.js
@@ -3,7 +3,7 @@ import * as api from "../api";
 
 export const login = createAsyncThunk(
   "auth/login",
-  async ({ formValue, navigate, toast }) => {
+  async ({ formValue, navigate, toast }, { rejectWithValue }) => {
     try {
       const response = await api.signIn(formValue);
       toast.success("Login Successfully");
@@ -11,6 +11,22 @@ export const login = createAsyncThunk(
       return response.data;
     } catch (err) {
       console.log(err);
+      return rejectWithValue(err.response.data);
+    }
+  }
+);
+
+export const register = createAsyncThunk(
+  "auth/register",
+  async ({ formValue, navigate, toast }, { rejectWithValue }) => {
+    try {
+      const response = await api.signUp(formValue);
+      toast.success("Register Successfully");
+      navigate("/");
+      return response.data;
+    } catch (err) {
+      console.log(err);
+      return rejectWithValue(err.response.data);
     }
   }
 );
@@ -32,6 +48,18 @@ const authSlice = createSlice({
       state.user = action.payload;
     },
     [login.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.payload.message;
+    },
+    [register.pending]: (state, action) => {
+      state.loading = true;
+    },
+    [register.fulfilled]: (state, action) => {
+      state.loading = false;
+      localStorage.setItem("profile", JSON.stringify({ ...action.payload }));
+      state.user = action.payload;
+    },
+    [register.rejected]: (state, action) => {
       state.loading = false;
       state.error = action.payload.message;
     },
