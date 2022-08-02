e1284668350543253494976f5897c8d50bf6e350
unknown <mdrakibulhoque83@gmail.com>
2022-08-02 23:20:06 +0600

    Header implement

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Header from "./components/Header";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { setUser } from "./redux/features/authSlice";
 
 function App() {
+  const dispatch = useDispatch();
+  const user = JSON.parse(localStorage.getItem("profile"));
+  useEffect(() => {
+    dispatch(setUser(user));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <BrowserRouter>
+      <Header />
       <ToastContainer />
       <h2>Hello Rakib</h2>
       <Routes>
