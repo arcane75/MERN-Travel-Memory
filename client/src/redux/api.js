5e4f4f723870b2a60f530285abac21d24bc73bc3
unknown <mdrakibulhoque83@gmail.com>
2022-08-04 23:56:31 +0600

    tourSlice,api implement for addupdate components

--- client/src/redux/api.js
+++ client/src/redux/api.js
@@ -2,5 +2,9 @@ import axios from "axios";
 
 const API = axios.create({ baseURL: "http://localhost:5000" });
 
+// Auth
 export const signIn = (formData) => API.post("/users/signin", formData);
 export const signUp = (formData) => API.post("/users/signup", formData);
+
+// Travel
+export const createTour = (tourData) => API.post("/tour", tourData);
