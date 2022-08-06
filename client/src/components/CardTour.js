f4d2ddb9c9b091a7a38a91c97489b8465f557dc7
unknown <mdrakibulhoque83@gmail.com>
2022-08-07 00:21:11 +0600

    travel post, card travel component written

--- client/src/components/CardTour.js
+++ client/src/components/CardTour.js
@@ -1,11 +1,53 @@
-import React from 'react';
+import React from "react";
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardImage,
+  MDBCardGroup,
+  MDBBtn,
+  MDBIcon,
+  MDBTooltip,
+} from "mdb-react-ui-kit";
+import { Link } from "react-router-dom";
 
-const CardTour = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const CardTour = ({ imageFile, _id, name, title, description, tags }) => {
+  const excerpt = (str) => {
+    if (str.length > 50) {
+      str = str.substring(0, 50) + " ...";
+    }
+    return str;
+  };
+  
+  return (
+    <MDBCardGroup>
+      <MDBCard className="h-100 mt-2 d-sm-flex" style={{ maxWidth: "20rem" }}>
+        <MDBCardImage
+          src={imageFile}
+          alt={title}
+          position="top"
+          style={{ maxWidth: "100%", height: "180px" }}
+        />
+
+        <div className="top-left">{name}</div>
+        <span className="text-start tag-card">
+          {tags.map((tag) => (
+            <></>
+          ))}
+        </span>
+
+        <MDBCardBody>
+          <MDBCardTitle className="text-start">{title}</MDBCardTitle>
+
+          <MDBCardText className="text-start">
+            {excerpt(description)}
+            <Link to={`/tour/${_id}`}>Read More</Link>
+          </MDBCardText>
+        </MDBCardBody>
+      </MDBCard>
+    </MDBCardGroup>
+  );
 };
 
 export default CardTour;
